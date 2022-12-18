54ee7127cc5b78bb3f9e3aa845cea1333f99a70f
Wdowik2115 <116074280+Wdowik2115@users.noreply.github.com>
2022-12-18 18:07:37 +0100

    Commit 5

---
+++ app/Models/People.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class People extends Model
+{
+    use HasFactory;
+    protected $fillable = ['Name','second_name','email','phone_number','country'];
+}
